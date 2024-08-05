414ea6dde7860dfe0986405e6891958ffe50d868
jChauhan18 <jlinux1999@gmail.com>
2024-08-05 23:42:06 +0530

    updated files

--- server.js
+++ server.js
@@ -15,9 +15,21 @@ app.get('/', function(req, res) {
     app.get('/(:id)', function(req, res) {
             res.render('pad');
         });
-        const sharejs = require("share")
-        require("redis")
-        
+
+    const sharejs = require("share")
+    require("redis")
+    
+    var redisClient;
+    console.log(process.env.REDISTOGO_URL);
+    if (process.env.REDISTOGO_URL) {
+      var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+      redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+      redisClient.auth(rtg.auth.split(":")[1]);
+    } else {
+      redisClient = require("redis").createClient();
+    }
+
+    
 const options = {
         db: {type: "redis"}
 }
