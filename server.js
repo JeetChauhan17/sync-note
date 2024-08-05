7d4c54647f19cb0ac46cacfe3e23d4d343b92993
jChauhan18 <jlinux1999@gmail.com>
2024-08-05 23:59:29 +0530

    fixed some bugs

--- server.js
+++ server.js
@@ -19,17 +19,17 @@ app.get('/', function(req, res) {
     const sharejs = require("share")
     require("redis")
     
-    var redisClient;
-    console.log(process.env.REDISTOGO_URL);
-    if (process.env.REDISTOGO_URL) {
-      var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-      redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-      redisClient.auth(rtg.auth.split(":")[1]);
-    } else {
-      redisClient = require("redis").createClient();
-    }
+    // var redisClient;
+    // console.log(process.env.REDISTOGO_URL);
+    // if (process.env.REDISTOGO_URL) {
+    //   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    //   redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+    //   redisClient.auth(rtg.auth.split(":")[1]);
+    // } else {
+    //   redisClient = require("redis").createClient();
+    // }
 
-    
+
 const options = {
         db: {type: "redis"}
 }
